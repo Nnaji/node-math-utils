c4b2665d0483481f56ca9cc2871e41f822105963
Kingsley Nnaji <kingsley.nnaji@gmail.com>
2019-02-05 18:45:34 +0100

    add function completed

---
+++ index.js
@@ -0,0 +1,11 @@
+const utils = require('./src/scripts/js/nmutils');
+let numArray = [1, 2, 3, 4, 5];
+
+console.log(utils.addn(numArray));
+console.log(utils.addn(1, 2, 3, 4, 5));
+console.log(utils.add(1, '2'));
+
+
+// let [one, two, three] = [1, 2, 3];
+
+// console.log(one, two, three);
