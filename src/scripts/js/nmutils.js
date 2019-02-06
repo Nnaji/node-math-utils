edc55cb251afbd433cc407c1864bb317162de343
Kingsley Nnaji <kingsley.nnaji@gmail.com>
2019-02-06 20:12:08 +0100

    added unit test for the add function

--- src/scripts/js/nmutils.js
+++ src/scripts/js/nmutils.js
@@ -1,6 +1,6 @@
 // Add function take two arguments of a number type values and return a single number value of their sum
 function add(numa, numb) {
-    // this sounds like a good idea to be very strict  in type checking in javascript
+    // this sounds like a good idea to be very strict in type checking in javascript
     // I am going to comment it out for now and will comeback to it later in the functional programming path of this project.
     // Javascript doest not care about type and you will see one of the reasons.
     // if (typeof numa | numb !== 'number') {
@@ -12,10 +12,11 @@ function add(numa, numb) {
 // Addn function takes a variable length of 
 function addn(...args) {
     let result = 0;
-    let i = 1;
-    for (i; i < args.length; i = i + 1) {
+    let i = 0;
+    for (i of args) {
         result = result + i;
     }
     return result;
 }
+
 module.exports = { addn, add };
