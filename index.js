5500dd07797f04323c087368653018b4a5a0233a
Kingsley Nnaji <kingsley.nnaji@gmail.com>
2020-04-06 09:36:03 +0200

    Vulnerability fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let numArray = [1, 2, 3, 4, 5];
 
 console.log(utils.addn(...numArray));
 console.log(utils.addn(1, 2, 3, 4));
-console.log(utils.add([1, 2, 3]));
+// console.log(utils.add([1, 2, 3]));
 
 // let [one, two, three] = [1, 2, 3];
 
